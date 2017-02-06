006fee8409a64d734729da9837d02e287eef4992
Shimrra <YuuzhanVong@gmx.de>
2017-02-06 07:35:29 +0100

    Cleaned utility of obsolete autodesk code.

--- Utility.cpp
+++ Utility.cpp
@@ -11,50 +11,6 @@ VertFaceRelation::VertFaceRelation(short orgID_)
 {
 }
 
-// Add a normal to the list if the smoothing group bits overlap,
-// otherwise create a new vertex normal in the list
-void VNormal::AddNormal(Point3 &n,DWORD s) {
-   if (!(s&smooth) && init) {
-     if (next) next->AddNormal(n,s);
-     else {
-      next = new VNormal(n,s);
-     }
-   }
-   else {
-     norm += n;
-     smooth |= s;
-     init = TRUE;
-   }
-}
- 
-// Retrieves a normal if the smoothing groups overlap or there is
-// only one in the list
-Point3 &VNormal::GetNormal(DWORD s)
-{
-   if (smooth&s || !next) return norm;
-   else return next->GetNormal(s); 
-}
- 
-// Normalize each normal in the list
-void VNormal::Normalize() {
-   VNormal *ptr = next, *prev = this;
-   while (ptr)
-   {
-     if (ptr->smooth&smooth) {
-      norm += ptr->norm;
-      prev->next = ptr->next;
-       delete ptr;
-      ptr = prev->next;
-     }
-     else {
-      prev = ptr;
-      ptr = ptr->next;
-     }
-   }
-   norm = ::Normalize(norm);
-   if (next) next->Normalize();
-}
-
 void Utility::ComputeFaceNormals(Mesh *mesh, std::vector<Point3> &normals)
 {
    normals.resize(mesh->getNumFaces());
--- Utility.h
+++ Utility.h
@@ -10,26 +10,6 @@ public:
 	short orgID;
 	int faceID;
 };
-
-//Normal generation copied from:
-//http://docs.autodesk.com/3DSMAX/16/ENU/3ds-Max-SDK-Programmer-Guide/index.html?url=files/GUID-9B83270D-1D20-4613-AB35-5EE9832C162E.htm,topicNumber=d30e24073
-
-class VNormal
-{
-   public:
-     Point3 norm;
-     DWORD smooth;
-     VNormal *next;
-     BOOL init;
- 
-     VNormal() {smooth=0;next=NULL;init=FALSE;norm=Point3(0,0,0);}
-     VNormal(Point3 &n,DWORD s) {next=NULL;init=TRUE;norm=n;smooth=s;}
-     ~VNormal() {delete next;}
-     void AddNormal(Point3 &n,DWORD s);
-     Point3 &GetNormal(DWORD s);
-     void Normalize();
-};
-
 class Utility
 {
 public:
